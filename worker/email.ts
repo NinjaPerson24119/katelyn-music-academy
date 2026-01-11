3c278194db3677024b8601a80ba944904c013a7e
Nicholas Wengel <ninjaperson24119@gmail.com>
2026-01-10 22:10:37 -0700

    fix: build

--- worker/email.ts
+++ worker/email.ts
@@ -92,7 +92,7 @@ export async function onRequestPostContactForm(request: any, env: Env) {
 async function sendEmail(body: any, kind: string, env: Env): Promise<Response> {
   const resend = new Resend(env.RESEND_API_KEY);
 
-  const { data, error } = await resend.emails.send(body);
+  const { error } = await resend.emails.send(body);
   if (error !== null) {
     console.log(
       `Failed to send email (${kind}), error: ${error}`,
